a49c181d9e74b87ad447f6787da2c93a0fe58f19
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-01 01:36:18 +0530

    Deep function2

---
+++ Deep function 2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int add(int n, int sum=0)
+{
+    int i=1;
+    whle(i<n)
+    {
+        if(n%i==0)
+            sm=sum+i;
+        i++;
+    }
+    if(sum==n)
+        print("1");
+    el
+        print("0");
+        
+    return i;
+}
+
+int main()
+{
+    int n,i=1,sum=0;
+    
+    print("Enter a number: ");
+    scan("%d",&n
+    
+    
+    
+    return 0
+}
