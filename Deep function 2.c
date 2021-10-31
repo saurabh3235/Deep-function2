a1f53a686249f5f134bd21d2c083da1af25cb245
saurabh3235 <72140254+saurabh3235@users.noreply.github.com>
2021-10-31 20:20:39 +0530

    this is correct program to fing perfect number

--- Deep function 2.c
+++ Deep function 2.c
@@ -1,29 +1,27 @@
+//program for finding perfect numbers
 #include<stdio.h>
 #include<conio.h>
+  int perfect(int);
+  int main(){
+          int num,s;
+          clrscr();
+          printf("Give an integer number: ");
+          scanf("%d",&num);
+          s = perfect(num);
+          if(s == num)
+          printf("\nThe given number %d is a perfect number",num);
+          else
+          printf("\nThe given number %d is not a perfect number",num);
+          getch();
+          return 0;
+          }
 
-int add(int n, int sum=0)
-{
-    int i=1;
-    while(i<n)
-    {
-        if(n%i==0)
-            sum=sum+i;
-        i++;
-    }
-    if(sum==n)
-        printf("1");
-    else
-        printf("0");
-        
-    return i;
-}
-
-int main()
-{
-    int n,i=1,sum=0;
-    
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    
-    return 0
-}
+  int perfect(int numbr){
+          int a=1, sum=0;
+          while(a< numbr){
+          if(numbr % a == 0)
+          sum=sum+a;
+          a++;
+          }
+          return(sum);
+          }
